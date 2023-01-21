3bb9ab691d90d26bd34942ead0df336febc29040
Vasiliy <mr.v.m.denisov@mail.ru>
2023-01-21 16:48:55 +0300

    clean

--- config/dwmblocks/blocks.def.h
+++ config/dwmblocks/blocks.def.h
@@ -1,17 +1,14 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/ /*Command*/	 	                            /*Update Interval*/	/*Update Signal*/
-    {" 🐧 ", "/opt/dwmblocks-distrotube/scripts/kernel",	 360,		          2},
-
-	{" 🔺 ", "/opt/dwmblocks-distrotube/scripts/upt",		 60,		          2},
-
-	{" 📦 ", "/opt/dwmblocks-distrotube/scripts/pacupdate",  360,		          9},
 	
-	{" 💻 ", "/opt/dwmblocks-distrotube/scripts/memory",	 6,		              1},
-
-	{" 🔊 ", "/opt/dwmblocks-distrotube/scripts/volume",     0,		              10},
-
-	{" 🕑 ", "/opt/dwmblocks-distrotube/scripts/clock",	     60,	              0},
+	{"", "$HOME/.config/dwmblocks/scripts/layout",	                 10,     	           10},
+	{"", "$HOME/.config/dwmblocks/scripts/bluetooth_headphones",	    1,		              5},
+	{"", "$HOME/.config/dwmblocks/scripts/spoti",	                    1,		              1},
+	{"", "$HOME/.config/dwmblocks/scripts/wireless_keyboard",	       60,		              1},
+	{"", "$HOME/.config/dwmblocks/scripts/memory",	                  6,		              1},
+	{"", "$HOME/.config/dwmblocks/scripts/volume",                    0,		              9},
+	{"", "$HOME/.config/dwmblocks/scripts/clock",	                    1,   	              0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
